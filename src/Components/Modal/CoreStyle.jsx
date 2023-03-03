505cdce61cd564a32302015abaa8d2c87b419f07
Gonzalo <mancebo.gonzalo27@gmail.com>
2023-03-03 13:29:18 -0300

    cambios

--- src/Components/Modal/CoreStyle.jsx
+++ src/Components/Modal/CoreStyle.jsx
@@ -1,78 +1,21 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-  container: {
-    display: "flex",
+  modalContainer: {
     flexDirection: "row",
-    flexWrap: "nowrap",
-    padding: 10,
-    paddingTop: 15,
-    paddingBottom: 15,
-    justifyContent: "center",
-  },
-
-  column: {
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    width: "60%",
-    height: 150,
-  },
-
-  firstColumn: {
-    flexDirection: "column",
+    backgroundColor: "blue"
+    },
+  card: {
     justifyContent: "center",
     alignItems: "center",
-    marginRight: 10,
-    width: "40%",
-    height: 150,
-  },
-  image: {
-    width: 100,
-    height: 100,
-    borderRadius: 5,
-  },
-  emoji: {
-    marginRight: 20,
-  },
-  card: {
-    borderBottomWidth: 2,
-    borderBottomColor: "black",
+    flex: 1,
+    borderWidth: 2,
     borderStyle: "solid",
-    paddingBottom: 10,
-    height: 170,
-  },
-
-  delete: {
-    height: 35,
-    width: 35,
-    marginLeft: 150,
-    marginBottom: 80,
-    justifyContent: "flex-end",
-  },
-  addcontainer: {
-    flex: 1,
-    flexDirection: "row",
-  },
-  finishproduct: {
-    marginTop: 10,
-    flex: 1,
-  },
-  addproduct: {
-    marginTop: 10,
-    flex: 1,
-  },
-  text: {
-    height: 50,
-    width: 130,
-    marginTop: 60,
-    fontWeight: "bold",
-  },
-  title: {
-    fontWeight: "bold",
-    marginTop: 6,
-  },
-  total: {
-    fontWeight: "bold",
-    marginTop: 10,
+    padding: 10,
+    height: "20%",
+    backgroundColor: "red",
+    borderRadius: 100,
+    height: 70,
+    width: 250,
   },
 });
